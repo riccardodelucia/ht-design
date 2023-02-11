ee135a8112b2c581cc9ffc896af6b329667cf082
riccardo.delucia <riccardo.delucia@fht.org>
2023-02-11 10:59:05 +0100

    inputs styling and fonts improvements

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import "@fontsource/poppins";
-import "@fontsource/noto-sans";
-import "@fontsource/open-sans";
+import "@fontsource/poppins/600.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/500.css";
+import "@fontsource/open-sans/700.css";
 import "./style/main.css";
