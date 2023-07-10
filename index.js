aa7094bfe8302fd423fdf2a264d42283ba65c0ef
Riccardo Roberto De Lucia <riccardo.delucia@fht.org>
2023-07-10 12:10:38 +0200

    Open props and improvements

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-import "@fontsource/poppins/600.css";
-import "@fontsource/raleway/300.css";
-import "@fontsource/raleway/400.css";
-import "@fontsource/raleway/500.css";
-import "@fontsource/raleway/700.css";
 import "./style/main.css";
